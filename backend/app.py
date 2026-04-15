24dc0c1d3691f05bd3526348697d0301d9d0f2c7
a <aljawhrah.sh@gmail.com>
2026-04-15 05:20:37 +0300

    Phase 1 complete — Flask server serving all screens

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
+
+SCREENS = os.path.join(os.path.dirname(__file__), '..', 'screens')
 
 #routing/mapping homepage url
 @app.route('/')
 def home():
-    return "CausalityAI server is running"
+    return send_from_directory(SCREENS, '00-login.html')
 
-#debug ture -> server restart automatically, port 5000 -> run http://localhost:5000
+#filename ->whatever html inside screens gets in the function 
+#http://127.0.0.1:5000/screens/any html file
+@app.route('/screens/<filename>')
+def serve_screen(filename):
+    return send_from_directory(SCREENS, filename)
+
+#debug ture -> server restart automatically, port 5000 -> run http://127.0.0.1:5000
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-    
